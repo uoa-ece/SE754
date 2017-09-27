827fa0f5e21a773e319d893d3ef1df0313c68f58
Sean Zhang <xuyun.zhang@auckland.ac.nz>
2017-09-27 23:56:05 +1300

    Add code for throwing exception based on X position (Red)

--- src/main/java/nz/ac/auckland/se754/TicTacToe.java
+++ src/main/java/nz/ac/auckland/se754/TicTacToe.java
@@ -3,6 +3,8 @@ package nz.ac.auckland.se754;
 public class TicTacToe {
 
 	public void play(int xPosition, int yPosition) {
-		throw new RuntimeException();
+		if(xPosition < 1 || xPosition > 3) {
+			throw new RuntimeException("X position is outside board!");
+		}
 	}
 }
--- src/test/java/nz/ac/auckland/se754/TicTacToeSpec.java
+++ src/test/java/nz/ac/auckland/se754/TicTacToeSpec.java
@@ -3,6 +3,7 @@ package nz.ac.auckland.se754;
 import static org.junit.Assert.*;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class TicTacToeSpec {
@@ -19,6 +20,7 @@ public class TicTacToeSpec {
 		ticTacToe.play(5, 2);
 	}
 	
+//	@Ignore  // Regression testing without this test
 	@Test(expected = RuntimeException.class)
 	public void shouldThrowRuntimeExceptionWhenYPlacedOutsideBoard() {
 		ticTacToe.play(2, 5);
