de5a950ff2fb17e52c8f4d7f8fa7f6ec966b4479
Sean Zhang <xuyun.zhang@auckland.ac.nz>
2017-09-28 00:05:26 +1300

    Refactoring by adding checkPosition() method (Green)

--- src/main/java/nz/ac/auckland/se754/TicTacToe.java
+++ src/main/java/nz/ac/auckland/se754/TicTacToe.java
@@ -3,11 +3,13 @@ package nz.ac.auckland.se754;
 public class TicTacToe {
 
 	public void play(int xPosition, int yPosition) {
-		if(xPosition < 1 || xPosition > 3) {
-			throw new RuntimeException("X position is outside board!");
-		}
-		if(yPosition < 1 || yPosition > 3) {
-			throw new RuntimeException("Y position is outside board!");
+		checkPosition('X', xPosition);
+		checkPosition('Y', yPosition);
+	}
+	
+	private void checkPosition(char axis, int position) {
+		if(position < 1 ||position > 3) {
+			throw new RuntimeException(axis+" position is outside board!");
 		}
 	}
 }
