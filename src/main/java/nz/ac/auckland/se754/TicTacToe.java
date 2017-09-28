85987b74a6e2cd0b76763c65df2d3bc38b5f765e
Sean Zhang <xuyun.zhang@auckland.ac.nz>
2017-09-28 19:01:43 +1300

    Refactoring the thrown exception with message

--- src/main/java/nz/ac/auckland/se754/TicTacToe.java
+++ src/main/java/nz/ac/auckland/se754/TicTacToe.java
@@ -3,13 +3,6 @@ package nz.ac.auckland.se754;
 public class TicTacToe {
 
 	public void play(int xPosition, int yPosition) {
-		checkPosition('X', xPosition);
-		checkPosition('Y', yPosition);
-	}
-	
-	private void checkPosition(char axis, int position) {
-		if(position < 1 ||position > 3) {
-			throw new RuntimeException(axis+" position is outside board!");
-		}
+		throw new RuntimeException("X position is outside board!");
 	}
 }
--- src/test/java/nz/ac/auckland/se754/TicTacToeSpec.java
+++ src/test/java/nz/ac/auckland/se754/TicTacToeSpec.java
@@ -3,7 +3,6 @@ package nz.ac.auckland.se754;
 import static org.junit.Assert.*;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 public class TicTacToeSpec {
@@ -18,12 +17,6 @@ public class TicTacToeSpec {
 	@Test(expected = RuntimeException.class)
 	public void shouldThrowRuntimeExceptionWhenXPlacedOutsideBoard() {
 		ticTacToe.play(5, 2);
-	}
-	
-//	@Ignore  // Regression testing without this test
-	@Test(expected = RuntimeException.class)
-	public void shouldThrowRuntimeExceptionWhenYPlacedOutsideBoard() {
-		ticTacToe.play(2, 5);
 	}
 
 }
