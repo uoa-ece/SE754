c470aa3d241cfd6c4807ad6a144408097478ac0a
Sean Zhang <xuyun.zhang@auckland.ac.nz>
2017-09-28 19:38:59 +1300

    Modify code by adding board data structure, turn green

--- src/main/java/nz/ac/auckland/se754/TicTacToe.java
+++ src/main/java/nz/ac/auckland/se754/TicTacToe.java
@@ -2,6 +2,11 @@ package nz.ac.auckland.se754;
 
 public class TicTacToe {
 
+	private char[][] board = {
+			{'\0', '\0', '\0'}, 
+			{'\0', '\0', '\0'}, 
+			{'\0', '\0', '\0'}};
+	
 	public void play(int xPosition, int yPosition) {
 		if(yPosition < 1 || yPosition > 3) {
 			throw new RuntimeException("Y position is outside board!");
@@ -9,6 +14,11 @@ public class TicTacToe {
 		if(xPosition < 1 || xPosition > 3) {
 			throw new RuntimeException("X position is outside board!");
 		}
-		throw new RuntimeException("Space is occupied!");
+		if(board[xPosition-1][yPosition-1] == '\0') {
+			board[xPosition-1][yPosition-1] = 'X';
+		}
+		else {
+			throw new RuntimeException("Space is occupied!");
+		}
 	}
 }
