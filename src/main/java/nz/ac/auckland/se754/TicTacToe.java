b4f47da1df1c6e1a60f5ac6c4245443b8afb49db
Sean Zhang <xuyun.zhang@auckland.ac.nz>
2017-09-28 19:14:13 +1300

    Add code to make test case for Y position exception pass

--- src/main/java/nz/ac/auckland/se754/TicTacToe.java
+++ src/main/java/nz/ac/auckland/se754/TicTacToe.java
@@ -3,6 +3,9 @@ package nz.ac.auckland.se754;
 public class TicTacToe {
 
 	public void play(int xPosition, int yPosition) {
+		if(yPosition < 1 || yPosition > 3) {
+			throw new RuntimeException("Y position is outside board!");
+		}
 		throw new RuntimeException("X position is outside board!");
 	}
 }
